115b2eed8dcbecb21ed81503ae7f596fb618ac43
TheStinkySigma <aleksandrovamariia03@gmail.com>
2024-06-16 11:34:32 -0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+meme_dict = {
+            "КРИНЖ": "Что-то очень странное или стыдное",
+            "ЛОЛ": "Что-то очень смешное",
+            "ROFL": "Uh, something",
+            "ROCK": "rock...."
+            }
+while True:
+    word = input("Введите непонятное слово (большими буквами!): ")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("THERE IS NO SUCH WORD YOU STINKY")
